8f96df36e0234053730f608a5389a42a34ffd2b1
KUYU <134703137+MCPRniconico@users.noreply.github.com>
2025-04-18 13:57:10 +0900

    Create GrasslessdirtBlock.javaGrasslessdirtBlock.java

---
+++ src/main/java/oldcraft/block/GrasslessdirtBlock.javaGrasslessdirtBlock.java
@@ -0,0 +1,29 @@
+
+package oldcraft.block;
+
+import net.minecraftforge.common.IPlantable;
+
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.core.Direction;
+import net.minecraft.core.BlockPos;
+
+public class GrasslessdirtBlock extends Block {
+	public GrasslessdirtBlock() {
+		super(BlockBehaviour.Properties.of().mapColor(MapColor.DIRT).sound(SoundType.GRAVEL).strength(1f, 10f));
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 15;
+	}
+
+	@Override
+	public boolean canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction direction, IPlantable plantable) {
+		return true;
+	}
+}
